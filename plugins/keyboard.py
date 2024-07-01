cb76bd8cbdf47e7551ed40e5d04a9f2a2137751d
Tom Parker-Shemilt <palfrey@tevp.net>
2024-07-01 22:00:15 +0100

    Support my new mac keyboard

--- plugins/keyboard.py
+++ plugins/keyboard.py
@@ -10,6 +10,13 @@ class Keyboard(Enum):
     UNKNOWN = 3
 
 
+MAC_KEYBOARDS = [
+    ("05ac", "0250"),  # Matias
+    ("05ac", "0221"),  # Matias
+    ("3434", "0281"),  # Keychron K8
+]
+
+
 class Plugin(UdevPlugin):
     keyboard_state: Keyboard = Keyboard.UNKNOWN
 
@@ -19,41 +26,48 @@ class Plugin(UdevPlugin):
     def setup(self):
         self.set_keyboard_state()
 
+    def set_as_mac(self):
+        if self.keyboard_state != Keyboard.MAC:
+            print("Setting keyboard state to Mac")
+            try:
+                subprocess.check_output(
+                    [
+                        "setxkbmap",
+                        "-model",
+                        "macbook79",
+                        "-layout",
+                        "gb",
+                        "-verbose",
+                        "10",
+                    ]
+                )
+                self.keyboard_state = Keyboard.MAC
+                self.parent.on_change()
+            except subprocess.CalledProcessError as e:
+                print("Can't currently set keyboard as setxkbmap failed")
+                print(e.output)
+
+    def set_as_pc(self):
+        if self.keyboard_state != Keyboard.PC:
+            # Default laptop keyboard
+            print("Setting keyboard state to PC")
+            subprocess.check_call(
+                ["setxkbmap", "-model", "pc104", "-layout", "gb", "-verbose", "10"]
+            )
+            self.keyboard_state = Keyboard.PC
+            self.parent.on_change()
+
     def set_keyboard_state(self):
         for device in self.udev_context.list_devices(subsystem="usb"):
-            if device.properties.get(
-                "ID_VENDOR_ID"
-            ) == "05ac" and device.properties.get("ID_MODEL_ID") in ["0250", "0221"]:
-                # Matias keyboard
-                if self.keyboard_state != Keyboard.MAC:
-                    print("Setting keyboard state to Mac")
-                    try:
-                        subprocess.check_output(
-                            [
-                                "setxkbmap",
-                                "-model",
-                                "macbook79",
-                                "-layout",
-                                "gb",
-                                "-verbose",
-                                "10",
-                            ]
-                        )
-                        self.keyboard_state = Keyboard.MAC
-                        self.parent.on_change()
-                    except subprocess.CalledProcessError as e:
-                        print("Can't currently set keyboard as setxkbmap failed")
-                        print(e.output)
-                break
-        else:
-            if self.keyboard_state != Keyboard.PC:
-                # Default laptop keyboard
-                print("Setting keyboard state to PC")
-                subprocess.check_call(
-                    ["setxkbmap", "-model", "pc104", "-layout", "gb", "-verbose", "10"]
-                )
-                self.keyboard_state = Keyboard.PC
-                self.parent.on_change()
+            for vendor, model in MAC_KEYBOARDS:
+                if (
+                    device.properties.get("ID_VENDOR_ID") == vendor
+                    and device.properties.get("ID_MODEL_ID") == model
+                ):
+                    self.set_as_mac()
+                    return
+
+        self.set_as_pc()
 
     def log_event(self, action, device):
         self.set_keyboard_state()
